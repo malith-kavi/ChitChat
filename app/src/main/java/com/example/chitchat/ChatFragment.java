0c9331fc53bee3ee795d5365508d89f84aed262b
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2025-01-18 17:49:36 +0530

    Update ChatFragment.java

--- app/src/main/java/com/example/chitchat/ChatFragment.java
+++ app/src/main/java/com/example/chitchat/ChatFragment.java
@@ -3,6 +3,7 @@ package com.example.chitchat;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,8 @@ import android.view.ViewGroup;
 
 public class ChatFragment extends Fragment {
 
+    RecyclerView recyclerView;
+
 
     public ChatFragment() {
     }
@@ -18,6 +21,10 @@ public class ChatFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        View view = inflater.inflate(R.layout.fragment_chat,container,false);
+        recyclerView = view.findViewById(R.id.recycler_view);
+        setupRecycleView();
+
+        return view;
     }
 }
