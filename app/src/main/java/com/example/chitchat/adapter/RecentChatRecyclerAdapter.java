1c6de6525c74878ca7ba58089d802b0436b4a636
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2025-01-18 17:49:29 +0530

    Create RecentChatRecyclerAdapter.java

---
+++ app/src/main/java/com/example/chitchat/adapter/RecentChatRecyclerAdapter.java
@@ -0,0 +1,59 @@
+package com.example.chitchat.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.chitchat.ChatActivity;
+import com.example.chitchat.R;
+import com.example.chitchat.model.ChatroomModel;
+import com.example.chitchat.utils.AndroidUtil;
+import com.example.chitchat.utils.FirebaseUtil;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+
+import org.w3c.dom.Text;
+
+public class RecentChatRecyclerAdapter extends FirestoreRecyclerAdapter<ChatroomModel, RecentChatRecyclerAdapter.ChatroomModelViewHolder> {
+
+    Context context;
+
+    public RecentChatRecyclerAdapter(@NonNull FirestoreRecyclerOptions<ChatroomModel> options, Context context) {
+        super(options);
+        this.context = context;
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull ChatroomModelViewHolder holder, int position, @NonNull ChatroomModel model) {
+
+    }
+
+    @NonNull
+    @Override
+    public ChatroomModelViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.recent_chat_recycle_row,parent,false);
+        return new ChatroomModelViewHolder(view);
+    }
+
+    class ChatroomModelViewHolder extends RecyclerView.ViewHolder{
+        TextView usernameText;
+        TextView lastMessageText;
+        TextView lastMessageTime;
+        ImageView profilePic;
+
+        public ChatroomModelViewHolder(@NonNull View itemView) {
+            super(itemView);
+            usernameText = itemView.findViewById(R.id.user_name_text);
+            lastMessageText = itemView.findViewById(R.id.last_message_text);
+            lastMessageTime = itemView.findViewById(R.id.last_message_time_text);
+            profilePic = itemView.findViewById(R.id.profile_pic_image_view);
+        }
+    }
+}
