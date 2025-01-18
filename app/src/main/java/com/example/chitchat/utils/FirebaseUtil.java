4b7c93b80fb7d7fc9acd689e6830f66273985890
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2025-01-18 17:19:11 +0530

    updates

--- app/src/main/java/com/example/chitchat/utils/FirebaseUtil.java
+++ app/src/main/java/com/example/chitchat/utils/FirebaseUtil.java
@@ -1,6 +1,7 @@
 package com.example.chitchat.utils;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -19,5 +20,25 @@ public class FirebaseUtil {
 
     public static DocumentReference currentUserDetails(){
         return FirebaseFirestore.getInstance().collection("users").document(currentUserId());
+    }
+
+    public static CollectionReference allUserCollectionReference(){
+        return FirebaseFirestore.getInstance().collection("users");
+    }
+
+    public static DocumentReference getChatroomReference(String chatroomId){
+        return FirebaseFirestore.getInstance().collection("chatrooms").document(chatroomId);
+    }
+
+    public static CollectionReference getChatroomMessageReference(String chatroomId){
+        return  getChatroomReference(chatroomId).collection("chats");
+    }
+
+    public  static  String getChatroomId(String userId1,String userId2){
+        if (userId1.hashCode()<userId2.hashCode()){
+            return  userId1+"_"+userId2;
+        }else {
+            return  userId2+"_"+userId1;
+        }
     }
 }
